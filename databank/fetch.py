02c3e4b75c5b74f858b6c81b38a4313404771ed9
Madhav <Madhavampire@gmail.com>
2024-04-28 15:04:43 +0530

    .

--- databank/fetch.py
+++ databank/fetch.py
@@ -7,7 +7,7 @@ import json
 URL = "https://www.artificialintelligence-news.com/artificial-intelligence-news"
 
 HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
 }
 
 response = requests.get(URL)
